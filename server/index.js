1a5fe95f1f7622feb8ca570daec90707e6f3c77d
Bonnie222 <616247581@qq.com>
2017-12-23 17:19:06 +0800

    增加server文件夹

---
+++ server/index.js
@@ -0,0 +1,29 @@
+//引入编写好的api
+const api = require('./api');
+//引入文件模块
+const fs = require('fs');
+//引入处理路径的模块
+const path = require('path');
+//引入处理post数据的模块
+const bodyParser = require('body-parser');
+//引入Express
+const express = require('express');
+const app = express();
+
+//解析application/json
+app.use(bodyParser.json());
+//解析application/x-www-form-urlencoded
+app.use(bodyParser.urlencodede({extended: false}));
+//访问静态资源文件 这里是访问所有dist目录下的静态资源文件
+app.use(express.static(path.resolve(__diname, '../dist')));
+//因为是单页面应用，所有请求都走/dist/index.html
+app.get('*', function(req,res){
+	const html = fs.readFileSync(path.resolve(__dirname, '../index.html'),'utf-8');
+	res.send(html);
+})
+
+//监听3000端口
+app.listen(3000,function(){
+	console.log('success listen to port 3000');
+})
+
--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ export default {
 }
 </script>
 
-<style lang="less">
+<style lang="less" rel="stylesheet/less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
 	.btn-wrap{
